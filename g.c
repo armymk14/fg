a4666e001b963f84cb95bb5e69c88791ef26d8e5
Ankush Kumar <ankushldh000@gmail.com>
2025-06-20 23:54:21 -0400

    pattern

--- g.c
+++ g.c
@@ -1,9 +1,10 @@
-#include<stdio.h>
-int main(){
-    int a,b,c;
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
     printf("enter two no. a and b");
-    scanf("%d %d",&a,&b);
-    c=a+b;
-    printf("sum of a and b is %d",c);
+    scanf("%d %d", &a, &b);
+    c = a + b;
+    printf("sum of a and b is %d", c);
     return 0;
 }
